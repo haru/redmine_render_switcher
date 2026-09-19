9c0aeb731b905c20453ed65393aa5d7f1338e79a
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2026-09-19 03:32:50 +0000

    add version.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
+require File.expand_path("version", __dir__)
+
 Redmine::Plugin.register :redmine_render_switcher do
   name 'Redmine Render Switcher plugin'
   author 'Author name'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version RedmineRenderSwitcher::VERSION
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
